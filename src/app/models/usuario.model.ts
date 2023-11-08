989f09f9a47bc89d2c7b785b4a086523f6bcb078
Nov3lo <69842707+Nov3lo@users.noreply.github.com>
2023-11-08 02:44:25 -0500

    video2

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -6,4 +6,14 @@ export class UsuarioModel {
   password: string;
   nombre: string;
 
+  constructor() {
+    this.email = ''; // Inicializar la propiedad "email" con un valor predeterminado
+    this.password = ''; // Inicializar la propiedad "password" con un valor predeterminado
+    this.nombre = ''; // Inicializar la propiedad "nombre" con un valor predeterminado
+  }
+
+
+
+
+
 }
